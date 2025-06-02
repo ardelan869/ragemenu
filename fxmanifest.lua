7afdbcac8ec41fa101a5b1bcb005073afebb7a22
ardelan <101870550+ardelan869@users.noreply.github.com>
2025-06-02 15:45:24 +0200

    fix: fix last commit message

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 
 author 'ardelan869'
-version 'chore: bump up version'
+version 'fix: fix last commit message'
 description 'FiveM native Rage Menu, made with React'
 
 ui_page 'web/dist/index.html'
