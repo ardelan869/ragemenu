ffec3497dd35f4bf4ba5f309f2281b41f347e228
ardelan <ardelanyamanel@outlook.de>
2025-06-02 15:41:46 +0200

    feat(web): add config for colors

---
+++ web/public/assets/config.js
@@ -0,0 +1,31 @@
+/**
+ * @type {Record<string, string>}
+ */
+window.colors = {
+  '~r~': 'text-red-500',
+  '~b~': 'text-blue-500',
+  '~g~': 'text-green-500',
+  '~y~': 'text-yellow-500',
+  '~m~': 'text-fuchsia-500',
+  '~c~': 'text-cyan-500',
+  '~w~': 'text-white',
+  '~k~': 'text-black',
+  '~a~': 'text-gray-500',
+  '~d~': 'text-gray-700',
+  '~l~': 'text-gray-300',
+  '~p~': 'text-purple-500',
+  '~o~': 'text-orange-500',
+  '~t~': 'text-teal-500',
+  '~v~': 'text-violet-500',
+  '~i~': 'text-indigo-500',
+  '~br~': 'text-amber-700',
+  '~li~': 'text-sky-400',
+  '~dg~': 'text-green-700',
+  '~lg~': 'text-green-400',
+  '~pe~': 'text-orange-200',
+  '~be~': 'text-amber-100',
+  '~sa~': 'text-red-300',
+  '~ma~': 'text-red-800',
+  '~pi~': 'text-pink-500',
+  '~ol~': 'text-olive-500'
+};
